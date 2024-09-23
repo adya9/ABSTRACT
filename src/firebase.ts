6cad5bdfd338e0442a3612364e0b65b88bc0bf2e
adya <adyakumari2001@gmail.com>
2024-09-23 22:13:31 +0530

    added login part

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+
+const app = getApps().length? getApp(): initializeApp(firebaseConfig);
+export {app}
